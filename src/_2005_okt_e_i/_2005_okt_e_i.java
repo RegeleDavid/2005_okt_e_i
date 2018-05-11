1e50776baa7fe21b5468f7c62bc6d81f2fce29a1
Regele <regele.david@gmail.com>
2018-05-11 15:27:24 +0200

    korep

---
+++ src/_2005_okt_e_i/_2005_okt_e_i.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _2005_okt_e_i;
+import java.io.*;
+/**
+ *
+ * @author Dávid
+ */
+public class _2005_okt_e_i {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+       String szo="asdfghjkléáéuztrew",szo2="errt", nyilt="", kulcs="";
+        for (int i = 0; i < szo.length(); i++) {
+            if('á'==szo.charAt(i)){
+                nyilt+="a";
+            }else
+            if('é'==szo.charAt(i)){
+                nyilt+="e";
+            }else
+            if('ű'==szo.charAt(i) || 'ú'==szo.charAt(i)){
+                nyilt+="u";
+            }else
+            if('ó'==szo.charAt(i) || 'ő'==szo.charAt(i)){
+                nyilt+="o";
+            }else{
+                 nyilt+=szo.charAt(i)+"";
+            }
+           
+            
+        }
+        nyilt=nyilt.toUpperCase();
+        int hossz_kulcs=szo2.length();
+        for (int i = 0; i < szo.length(); i++) {
+            kulcs+=szo2.charAt(i%hossz_kulcs)+"";
+        }
+        kulcs=kulcs.toUpperCase();
+        System.out.println(nyilt);
+        System.out.println(kulcs);
+        RandomAccessFile raf=new RandomAccessFile("Vtabla.dat", "r");
+        String sor=raf.readLine();
+        char[] tomb=sor.toCharArray();
+        int db=sor.length();
+        char[][] t=new char[db][db];
+        raf.seek(0);
+        db=0;
+        while((sor=raf.readLine())!=null){
+            for (int i = 0; i < sor.length(); i++) {
+                t[db][i]=sor.charAt(i);
+            }
+            db++;
+        }
+        for (char[] cs : t) {
+            for (char c : cs) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        raf.close();
+        String kod="";
+        for (int i = 0; i < nyilt.length(); i++) {
+            int s=0,o=0;
+            for (int j = 0; j < tomb.length; j++) {
+                if(tomb[j]==nyilt.charAt(i)){
+                    s=i;
+                    
+                }
+                if(tomb[j]==kulcs.charAt(i)){
+                    o=i;
+                }
+            }
+            kod+=t[s][o]+"";
+        }
+        
+        System.out.println(kod);
+    }
+    
+}
